be9456d5eb7cce8215ed9daeae4d91a1e8567deb
oravix27 <128840829+oravix27@users.noreply.github.com>
2024-02-21 12:45:31 -0500

    default MAUI project added

---
+++ BasketballGUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BasketballGUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
