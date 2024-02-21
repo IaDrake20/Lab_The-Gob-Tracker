2dd8126b54fd2e6e3ef8a56ccb8591aca76087c4
oravix27 <128840829+oravix27@users.noreply.github.com>
2024-02-21 16:05:18 -0500

    Added Quit Button

--- BasketballGUI/MainMenu.xaml.cs
+++ BasketballGUI/MainMenu.xaml.cs
@@ -6,4 +6,9 @@ public partial class MainMenu : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void btnQuit_Clicked(object sender, EventArgs e)
+    {
+        Application.Current.Quit();
+    }
 }
