4cef64e4383ba6a6a1e4c1ac08fe766589f30a15
oravix27 <128840829+oravix27@users.noreply.github.com>
2024-03-07 14:33:03 -0500

    Create Team Page Added

--- BasketballGUI/NewGame.xaml.cs
+++ BasketballGUI/NewGame.xaml.cs
@@ -2,8 +2,39 @@ namespace BasketballGUI;
 
 public partial class NewGame : ContentPage
 {
-	public NewGame()
+    public Color originalColor = Colors.Red;
+    public Color clickColor = Colors.White;
+    public NewGame()
 	{
 		InitializeComponent();
 	}
+
+    private async void btnStartScoring_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new LiveEnterStats());
+    }
+
+    private async void btnNewTeam_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new CreateTeam());
+    }
+
+    private void btnPressed(object sender, EventArgs e)
+    {
+        var button = sender as Button;
+        if (button != null)
+        {
+            originalColor = button.BackgroundColor;
+            button.BackgroundColor = clickColor;
+        }
+    }
+
+    private void btnReleased(object sender, EventArgs e)
+    {
+        var button = sender as Button;
+        if (button != null)
+        {
+            button.BackgroundColor = originalColor;
+        }
+    }
 }
