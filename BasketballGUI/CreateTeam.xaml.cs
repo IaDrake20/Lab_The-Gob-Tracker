3ef16d672e285de158784c88a7713b0265900604
oravix27 <128840829+oravix27@users.noreply.github.com>
2024-03-07 16:21:14 -0500

    Create Team page styled

--- BasketballGUI/CreateTeam.xaml.cs
+++ BasketballGUI/CreateTeam.xaml.cs
@@ -6,4 +6,29 @@ public partial class CreateTeam : ContentPage
 	{
 		InitializeComponent();
 	}
+    private void btnPressed(object sender, EventArgs e)
+    {
+        var button = sender as Button;
+        if (button != null)
+        {
+            originalColor = button.BackgroundColor;
+            button.BackgroundColor = clickColor;
+        }
+    }
+
+    private void btnReleased(object sender, EventArgs e)
+    {
+        var button = sender as Button;
+        if (button != null)
+        {
+            button.BackgroundColor = originalColor;
+        }
+    }
+    public Color originalColor = Colors.Red;
+    public Color clickColor = Colors.White;
+
+    private void btnAddTeam_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
