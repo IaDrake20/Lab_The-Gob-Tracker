5d460817939bdcb0442802584b94344974c2a599
IaDrake20 <ios.drake05@gmail.com>
2024-03-08 09:18:52 -0500

    added view

---
+++ API Gob Tracker/Controllers/StatValsPerGameController.cs
@@ -0,0 +1,47 @@
+﻿using API_Gob_Tracker.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Gob_Tracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatValsPerGameController : ControllerBase
+    {
+        private readonly GobTrackerContext _context;
+
+        public StatValsPerGameController(GobTrackerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/AllRawStats
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StatValsPerGame>>> GetStatsValsPerGame()
+        {
+            return await _context.StatValsPerGames.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<StatValsPerGame>>> GetStatsValsPerGame(int id)
+        {
+            if (_context.StatValsPerGames == null)
+            {
+                return NotFound();
+            }
+
+            var pstats = await _context.StatValsPerGames.ToListAsync();
+
+            if (pstats == null)
+            {
+                return NotFound();
+            }
+
+            //LINQ
+            var resultPlayerStats = pstats.Where(x => x.GameId == id).ToList();
+
+            return resultPlayerStats;
+        }
+    }
+}
---
+++ API Gob Tracker/Models/StatValsPerGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_Gob_Tracker.Models;
+
+public partial class StatValsPerGame
+{
+    public string StatName { get; set; } = null!;
+
+    public decimal? StatValue { get; set; }
+
+    public int? TeamId { get; set; }
+
+    public int? GameId { get; set; }
+}
