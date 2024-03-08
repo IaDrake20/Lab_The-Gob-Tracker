113a7cb57e9cc98295ad25604341073a84aa61bc
Mscar1235 <129341137+Mscar1235@users.noreply.github.com>
2024-03-08 08:51:07 -0500

    View Live Game

--- BasketballGUI/LiveGame.xaml.cs
+++ BasketballGUI/LiveGame.xaml.cs
@@ -7,13 +7,79 @@ namespace BasketballGUI;
 
 public partial class LiveGame : ContentPage
 {
+    public int GameId;
+    public int T1Id;
+    public int T2Id;
+
 	public LiveGame(int gameID)
 	{
+        GameId = gameID;
         Debug.WriteLine(gameID);
         InitializeComponent();
         GetGameAsync(gameID);
         BindingContext = this;
-	}
+        KeepScoreAsync();
+    }
+
+
+    private async Task KeepScoreAsync()
+    {
+        while (true)
+        {
+            await Task.Delay(5000);
+            await GetScoreStatsAsync();
+        }
+    }
+
+
+    private async Task GetScoreStatsAsync()
+    {
+        string apiUrl = "https://localhost:7067/api/ScoringStat";
+
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonString = await response.Content.ReadAsStringAsync();
+
+                    List<ScoringStat> stats = JsonConvert.DeserializeObject<List<ScoringStat>>(jsonString);
+
+
+                    foreach (ScoringStat stat in stats)
+                    {
+                        if (stat.GameID == GameId)
+                        {
+                            if (stat.TeamID == T1Id)
+                            {
+                                Debug.WriteLine("Updating home score");
+                                lblHomeScore.Text = stat.TotalPtsMade.ToString();
+                            }
+                            else if (stat.TeamID == T2Id)
+                            {
+                                Debug.WriteLine("Updating away score");
+                                lblAwayScore.Text = stat.TotalPtsMade.ToString();
+                            }
+                        }
+                    }
+
+                }
+                else
+                {
+                    Debug.WriteLine("API request failed with status code:" + response.StatusCode);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
 
 
     private async Task GetGameAsync(int id)
@@ -35,7 +101,8 @@ public partial class LiveGame : ContentPage
                 {
                     string jsonString = await response.Content.ReadAsStringAsync();
                     game = JsonConvert.DeserializeObject<Game>(jsonString);
-
+                    T1Id = game.Team1Id;
+                    T2Id = game.Team2Id;
                     List<Team> teams = await GetTeamsAsync();
 
                     foreach (Team team in teams)
@@ -43,6 +110,7 @@ public partial class LiveGame : ContentPage
                         if(team.Id == game.Team1Id)
                         {
                             HTeam.Text = team.Name;
+
                         }
                         else if(team.Id == game.Team2Id)
                         {
